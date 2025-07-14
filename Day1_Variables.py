4c708b0f995557e407e53ca54cf16873103a6ce0
hachihan <pkmnz420@gmail.com>
2025-07-13 20:05:46 -0700

    Day 01: Variables basics

---
+++ Day1_Variables.py
@@ -0,0 +1,17 @@
+# What exactly is a variable?
+# Anything that stores something (even the value "None"
+# Variable names must:
+# start with a letter (a-z and A-Z) or underscore (_)
+# only contain letters, numbers or underscores
+# not be a Python keyword, like import# ##
+
+x=5
+y="y"
+z=1.2
+
+print(x,y,z)
+
+age=20
+Name="Pal"
+print(f" My name is {Name} and i am {age} years old")
+
