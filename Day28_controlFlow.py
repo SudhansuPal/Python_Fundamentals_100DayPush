b8df51d512e1fd432bec24709fcfe885d06ab344
hachihan <pkmnz420@gmail.com>
2025-08-11 17:56:24 -0700

    Day 28 commit.

---
+++ Day28_controlFlow.py
@@ -0,0 +1,11 @@
+for num in range(1, 8):
+    if num == 3:
+        pass  #add code here later
+        print(f"{num} will be handled")
+    elif num == 5:
+        print("Skipping number 5 entirely.")
+        continue  
+    else:
+        print(f"Processing number: {num}")
+
+print("Loop finished.")
