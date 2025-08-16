027b6aa381ac4fd8eabec0a86d7b4614ddff26fc
hachihan <pkmnz420@gmail.com>
2025-08-16 15:57:20 -0700

    Day 24 commit.

--- Day24_NestedFunctions.py
+++ Day24_NestedFunctions.py
@@ -9,4 +9,4 @@ print(f)
 # What we get back is a reference to the nested function built—the one created and assigned to action when the nested def runs. If we now call what we got back from the outer function:
 
 print(f(3))
-print(f(4))
+print(f)
